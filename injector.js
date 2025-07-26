201d720a2e06f4c4a106016992c9deb49fcac29e
zxcwxarghiugu4g4 <discordcuentaruben67@gmail.com>
2025-07-26 19:10:50 +0200

    Update injector.js

--- injector.js
+++ injector.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   const imagenes = [
-    "https://t-t-t-t-t-t-t-t-t-t-t-t-t-t-t-t-t-t-t.netlify.app/jajajaja/Imagen_de_WhatsApp_2025-07-26_a_las_01.22.22_0090782d.jpg"
+    "htt"
   ];
 
   const galeria = document.querySelector('.gallery');
